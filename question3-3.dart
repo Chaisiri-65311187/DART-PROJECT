c4edd1b496c63f96b4a68e2414c76958b5bfceda
Chaisiri-65311187 <ks.chaisiri46@gmail.com>
2024-11-25 15:51:19 +0700

    question3-3

--- question3-3.dart
+++ question3-3.dart
@@ -1,23 +1,23 @@
 import 'dart:io';
 
+// ฟังก์ชัน createUser
 void createUser({required String name, required int age, bool isActive = true}) {
-
   print('ชื่อผู้ใช้: $name');
   print('อายุ: $age');
   print('สถานะการใช้งาน: ${isActive ? 'ใช้งานอยู่' : 'ไม่ใช้งาน'}');
 }
 
 void main() {
-  stdout.write('กรุณาป้อนชื่อ: ');
+  // รับค่าจากคีย์บอร์ด
+  stdout.write('กรุณาป้อนชื่อผู้ใช้: ');
   String name = stdin.readLineSync()!;
 
   stdout.write('กรุณาป้อนอายุ: ');
   int age = int.parse(stdin.readLineSync()!);
 
-  stdout.write('ต้องการให้ผู้ใช้ใช้งานหรือไม่ (yes/no): ');
-  String? status = stdin.readLineSync();
+  // ค่าของ isActive ถูกตั้งเป็น true โดยค่าเริ่มต้น
+  bool isActive = true;
 
-  bool isActive = (status != null && status.toLowerCase() == 'yes');
-
+  // เรียกใช้งานฟังก์ชัน createUser
   createUser(name: name, age: age, isActive: isActive);
 }
